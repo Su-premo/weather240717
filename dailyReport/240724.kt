f78399cb044b74c9af33271557c1bb72e1caabb8
subin <aj1234@hotmail.co.kr>
2024-07-25 09:36:14 +0900

    240724: kts + xmls + errors

---
+++ dailyReport/240724.kt
@@ -0,0 +1,7 @@
+240717
+        1. api key를 local.properties에 추가
+        2. Modelweather, WeatherAdapter, WeatherInterface .kt 파일들 생성
+        3. content_main.xml 작성: 날씨 정보를 띄울 메인 화면
+        4. list_item_weather.xml 파일 생성: 세분화 날씨 정보를 담을 리스트 아이템
+        5. gradleLocalProperties(rootDir) 부분 에러 확인중(build.gradle)
+        6. GsonConverterFactory 부분 에러 확인중(WeatherInterface.kt)
